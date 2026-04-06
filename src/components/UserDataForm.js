ce8772a916ba0863f8e0d6c6bb7516a7eb225d95
Alex Brilevski <brilevski.alex@gmail.com>
2026-04-06 15:55:28 +0300

    Logic for retrieving the filled-out user data and submitting the form was implemented

--- src/components/UserDataForm.js
+++ src/components/UserDataForm.js
@@ -1,16 +1,37 @@
+import { useState } from 'react';
 import classes from './UserDataFrom.module.css';
 
 const UserDataForm = () => {
+  const [userData, setUserData] = useState({ name: '', age: '' });
+
+  const formSubmitHandler = (e) => {
+    e.preventDefault();
+
+    console.log(userData);
+  };
+
+  const inputChangeHandler = (fieldId, e) => {
+    setUserData(prevUserData => ({ ...prevUserData, [fieldId]: e.target.value }));
+  };
+
   return (
-    <form className={classes.userDataForm}>
+    <form onSubmit={formSubmitHandler} className={classes.userDataForm}>
       <div className={classes.formControls}>
         <div className={classes.formControl}>
           <label htmlFor="name">Name</label>
-          <input id="name" />
+          <input
+            id="name"
+            value={userData.name}
+            onChange={(e) => inputChangeHandler('name', e)}
+          />
         </div>
         <div className={classes.formControl}>
           <label htmlFor="age">Age</label>
-          <input id="age" />
+          <input
+            id="age"
+            value={userData.age}
+            onChange={(e) => inputChangeHandler('age', e)}
+          />
         </div>
       </div>
       <div className={classes.formActions}>
